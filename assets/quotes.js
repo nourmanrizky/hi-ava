abfe7f609b8ace0cfaf15a49a248fe202645036c
nourmanrizky <nourmanrizky@gmail.com>
2020-05-08 09:33:43 +0700

    new
    a

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "avrill.v", quotes: " Oh ya mala, Apapun yang terjadi, tetaplah bernapas ya :)" },
+    { author: "antjedtta_", quotes: "semangat ya buat jalani semua kegiatan nya \n Doa dan Usaha harus seimbang.." },
+    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU.. haha \n semoga mala gak paham..wkwk" },
+    { author: "standinshd", quotes: "Mala Jangan lupa bersyukur juga ya, sayangi dirimu dan keluargamu " },
+    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday :)." },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "standinshd", quotes: "hehe...terimakasih ya mala, sudah mau kenal dan sedekat ini :)" },
 ];
--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["Mamal..", "Mala Comel..", "Nurmala Sari..", "Cewe Yg Ngangenin.. :)", "Kakak Gemayb.."],
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
+    evening: "Selamat Malam",
+    afternoon: "Selamat Sore",
+    day: "selamat siang",
+    morning: "Selamat Pagi",
+    night: "Selamat Tidur"
   }
 };
