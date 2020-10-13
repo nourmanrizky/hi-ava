742eb4a6129d18c3f34429e4397eda7fdd59df96
nourmanrizky <64998888+nourmanrizky@users.noreply.github.com>
2020-10-13 12:19:58 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "Nourman", quotes: " Oh ya mala, Apapun yang terjadi, tetaplah bernapas ya :)" },
-    { author: "Nourman", quotes: "Semangat ya mala buat jalani semua kegiatan nya \n Doa dan Usaha harus seimbang.." },
-    { author: "Nourman", quotes: "Mala! I wuv chuuuu! UwU.. haha \n semoga mala gak paham..wkwk" },
-    { author: "Nourman", quotes: "Mala Jangan lupa bersyukur juga ya, sayangi dirimu dan keluargamu " },
+    { author: "Nourman", quotes: " Oh ya mala, Apapun yang terjadi, tetaplah tersenyum ya :)" },
+    { author: "Nourman", quotes: "Semangat ya mala buat jalani semua kegiatan nya \n kamu pasti bisa" },
+    { author: "Nourman", quotes: "Mala! I wuv chuuuu! UwU.. haha \n yang ini biar mala terhibur :) " },
+    { author: "Nourman", quotes: "Tetaplah jadi orang yang rendah hati, penyabar, dan punya integritas tinggi ya " },
     { author: "Nourman", quotes: "I may not with you everyday, but I love you everyday :)." },
-    { author: "Nourman", quotes: "I'm sorry for lov*ng you." },
-    { author: "Nourman", quotes: "hehe...terimakasih ya mala, sudah mau kenal dan sedekat ini :)" },
+    { author: "Nourman", quotes: "Semoga mala selalu dalam keadaan baik, \n dan bisa menjaga orang tua serta org yang mala sayang" },
+    { author: "Nourman", quotes: "Terimakasih ya mala, sudah mau kenal dan sedekat ini \n dan semoga bisa jadi bidan yang handal dan profesional" },
 ];
